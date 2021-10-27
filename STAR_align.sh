c52a2935bd811b66f3d7164bb07919d44064e83d
avi-si <66920222+avi-si@users.noreply.github.com>
2021-10-27 16:48:39 -0400

    Update STAR_align.sh

--- STAR_align.sh
+++ STAR_align.sh
@@ -6,15 +6,8 @@
 #SBATCH --mail-type ALL
 
 #A29
-#The below transcriptome was aligned on Thursday, 10/21 
-#A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L1_1.fq.gz
-#A29 
-FASTQL1="/workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L1_1.fq,/workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L3_1.fq"
-FASTQL3="/workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L1_2.fq,/workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L3_2.fq"
-OutDir="/home/mph75_0001/ams686/Spat_RNA_Seq/STAR/Toy_dir_out"
-OutDirUnm="/home/mph75_0001/ams686/Spat_RNA_Seq/STAR/Toy_dir_out_umn"
-GenomeDir="/workdir/ams686/Genome_dir"
-/programs/STAR-2.7.9a/bin/Linux_x86_64_static/STAR --quantMode GeneCounts --genomeDir $GenomeDir --runThreadN 3 --readFilesIn $FASTQL1 $FASTQL3 --outFileNamePrefix $OutDir --outFilterMultimapNmax 20* --outReadsUnmapped $OutDirUnm --outSAMtype BAM SortedByCoordinate 
+/programs/STAR-2.7.9a/bin/Linux_x86_64_static/STAR
+--quantMode GeneCounts --genomeDir $GenomeDir --runThreadN 3 --readFilesIn /workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L1_1.fq,/workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L3_1.fq /workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L1_1.fq,/workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L3_1.fq --outFileNamePrefix /home/mph75_0001/ams686/Spat_RNA_Seq/STAR/Toy_dir_out --outFilterMultimapNmax 20* --outReadsUnmapped /home/mph75_0001/ams686/Spat_RNA_Seq/STAR/Toy_dir_out_umn --outSAMtype BAM SortedByCoordinate 
 
 
 #HC5 
