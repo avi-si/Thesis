b47e8b4bd11bbf27e51c09f27fb4e4b43b0dafc8
avi-si <66920222+avi-si@users.noreply.github.com>
2021-10-27 17:00:53 -0400

    Update STAR_align.sh

--- STAR_align.sh
+++ STAR_align.sh
@@ -6,8 +6,10 @@
 #SBATCH --mail-type ALL
 
 #A29
-/programs/STAR-2.7.9a/bin/Linux_x86_64_static/STAR
---quantMode GeneCounts --genomeDir $GenomeDir --runThreadN 3 --readFilesIn /workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L1_1.fq,/workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L3_1.fq /workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L1_1.fq,/workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L3_1.fq --outFileNamePrefix /home/mph75_0001/ams686/Spat_RNA_Seq/STAR/Toy_dir_out --outFilterMultimapNmax 20* --outReadsUnmapped /home/mph75_0001/ams686/Spat_RNA_Seq/STAR/Toy_dir_out_umn --outSAMtype BAM SortedByCoordinate 
+/programs/STAR-2.7.9a/bin/Linux_x86_64_static/STAR --quantMode GeneCounts --genomeDir /workdir/ams686/Genome_dir --runThreadN 3 --readFilesIn /workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L1_1.fq,/workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L3_1.fq /workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L1_2.fq,/workdir/ams686/Toy_dir/A29_CKDL210006718-1a-AK1847-7UDI301_HYC5VDSXY_L3_2.fq --outFileNamePrefix /home/mph75_0001/ams686/Spat_RNA_Seq/STAR/Toy_dir_out --outFilterMultimapNmax 20* --outReadsUnmapped /home/mph75_0001/ams686/Spat_RNA_Seq/STAR/Toy_dir_out_umn --outSAMtype BAM SortedByCoordinate 
+
+#HC5
+/programs/STAR-2.7.9a/bin/Linux_x86_64_static/STAR --quantMode GeneCounts --genomeDir /workdir/ams686/Genome_dir --runThreadN 3 --readFilesIn /workdir/ams686/Toy_dir/HC5_CKDL210006718-1a-AK1878-AK844_HYC5VDSXY_L1_1.fq,/workdir/ams686/Toy_dir/HC5_CKDL210006718-1a-AK1878-AK844_HYC5VDSXY_L3_1.fq /workdir/ams686/Toy_dir/HC5_CKDL210006718-1a-AK1878-AK844_HYC5VDSXY_L1_2.fq,/workdir/ams686/Toy_dir/HC5_CKDL210006718-1a-AK1878-AK844_HYC5VDSXY_L3_2.fq --outFileNamePrefix /home/mph75_0001/ams686/Spat_RNA_Seq/STAR/Toy_dir_out --outFilterMultimapNmax 20* --outReadsUnmapped /home/mph75_0001/ams686/Spat_RNA_Seq/STAR/Toy_dir_out_umn --outSAMtype BAM SortedByCoordinate 
 
 
 #HC5 
